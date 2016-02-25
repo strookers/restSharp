771b442e5b899a23d92abe02f3f484454f737d37
Michael Kirkegaard <Michael Kirkegaard>
2016-02-25 13:40:59 +0100

    init commit

---
+++ BLL/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace BLL
+{
+    public class Class1
+    {
+        RestClient client = new RestClient();
+
+        public void RestSharp()
+        {
+            client.BaseUrl = new Uri("");
+        }
+        
+    }
+}
